f027b7db9e5de3b80507c519bc9abcc667ead89f
Monchito5 <guille2807lol@gmail.com>
2022-09-07 21:56:39 -0500

    Interpolación dd

--- Proyecto Aprender A/learnto.py
+++ Proyecto Aprender A/learnto.py
@@ -6,5 +6,17 @@ learntoApp = Flask(__name__)
 def index():
     return render_template('home.html')
 
+@learntoApp.route('/login')
+def index():
+    return render_template('login.html')
+
+@learntoApp.route('/user/<nombre>')
+def user(nombre):
+    data={
+        'titulo':'Usuario',
+        'nombre':nombre
+    }
+    return render_template('user.html', data=data)
+
 if __name__=='__main__':
     learntoApp.run(debug=True, port=3300)
