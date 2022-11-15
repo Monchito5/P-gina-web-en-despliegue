8bce26e2b0348b971cbb39768502a6f609971be8
Monchito5 <guille2807lol@gmail.com>
2022-11-14 23:15:52 -0600

    Cambios menores tt

--- Proyecto Aprender A/config.py
+++ Proyecto Aprender A/config.py
@@ -1,6 +1,12 @@
+import os
 class config:
-
     SECRET_KEY = 'monchito5'
+    MAIL_SERVER = 'smpt.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_SSL = False
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'learntoapplication@gmail.com'
+    MAIL_PASSWORD = os.environ.get('PASSWORD_EMAIL_CF')
 
 class DevelompentConfig(config):
     DEBUG = True
